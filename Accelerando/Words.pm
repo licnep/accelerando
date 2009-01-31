7936ea35754aa9cb9a14436ac795681a7565d047
Ben Crowell <githubcrowell09@lightandmatter.com>
2009-01-31 12:30:16 -0800

    add plumbing for internationalization; add French translation

---
+++ Accelerando/Words.pm
@@ -0,0 +1,8 @@
+package Accelerando::Words;
+use Locale::Maketext;
+use base qw(Locale::Maketext);
+our %Lexicon = (
+  '_AUTO' => 1,
+);
+  
+1; # sets _AUTO for base class
---
+++ Accelerando/Words/en.pm
@@ -0,0 +1,8 @@
+package Accelerando::Words::en;
+use base qw(Accelerando::Words);
+use utf8;
+our %Lexicon = (
+  '_AUTO' => 1,
+);
+
+1;
